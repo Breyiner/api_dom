f907ca9db339f306eecf982547e5a106034ea84e
Breyiner <breyneracosta7@gmail.com>
2025-05-07 20:44:25 -0500

    instalación de dependencias e inicialización del servidor

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ "extended": true }));
+
+app.listen(3000, () => {
+  console.log("Servidor en funcionamiento...");
+});
